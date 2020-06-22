08c00cb1afff63961ea75546519a5a7d2df715e0
atzandrew <atzandrew@gmail.com>
2020-06-22 05:11:03 -0700

    Added my "script"

---
+++ andrew.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+echo "I miss you guys & gals.  I wish I could have a do-over on this quarter if for no other reason than to spend a little more time with y'all.  We've got a lot of talent, and a lot of cool people in this class and I hope to see you guys in the industry.  Good luck!"
+echo "-Andrew"
